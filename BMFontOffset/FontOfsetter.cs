5483ae3add24268e9958928fbee666ed2a82b709
Justin Johnson <jjohnson@westedge.us>
2017-10-11 11:24:34 -0600

    Initial creation of simple font offset functionality

---
+++ BMFontOffset/FontOfsetter.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.RegularExpressions;
+
+namespace BMFontOffset
+{
+    public class FontOfsetter
+    {
+        public string SourceFontPath { get; set; }
+        public string DestinationFontPath { get; set; }
+        public int XOffset { get; set; }
+        public int YOffset { get; set; }
+
+
+        public FontOfsetter(string sourcePath, string destPath, int xOffset, int yOffset)
+        {
+            SourceFontPath = sourcePath;
+            DestinationFontPath = destPath;
+            XOffset = xOffset;
+            YOffset = yOffset;
+        }
+
+        public FontOfsetter()
+        {
+            // intentionally empty constructor
+        }
+
+        public void ProcessFontFile()
+        {
+            if (string.IsNullOrEmpty(SourceFontPath) || !File.Exists(SourceFontPath))
+            {
+                throw new Exception("Bad source font file path provided: " + SourceFontPath);
+            }
+
+            var fnt = File.ReadAllText(SourceFontPath);
+
+            // apply the x offsets
+            fnt = Regex.Replace(fnt, "x=([0-9]+)", delegate (Match match)
+            {
+                var xVal = Convert.ToInt16(match.Groups[1].Value);
+                var newX = xVal + XOffset;
+                return "x=" + newX.ToString();
+            });
+
+            // apply the y offsets
+            fnt = Regex.Replace(fnt, "y=([0-9]+)", delegate (Match match)
+            {
+                var yVal = Convert.ToInt16(match.Groups[1].Value);
+                var newY = yVal + YOffset;
+                return "y=" + newY.ToString();
+            });
+
+            if (string.IsNullOrWhiteSpace(DestinationFontPath))
+            {
+                throw new Exception("Bad or empty destination font file path provided.");
+            }
+
+            // write the file
+            File.WriteAllText(DestinationFontPath, fnt);
+        }
+
+
+    }
+}
---
+++ BMFontOffset/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace BMFontOffset
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                string srcFile = Convert.ToString(args[0]);
+                int xOffset = Convert.ToInt16(args[1]);
+                int yOffset = Convert.ToInt16(args[2]);
+                string destFile = Convert.ToString(args[3]);
+
+                var offsetter = new FontOfsetter(srcFile, destFile, xOffset, yOffset);
+
+                Console.WriteLine("Processing font file.");
+                offsetter.ProcessFontFile();
+                Console.WriteLine("Processing complete, file created at: " + destFile);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Bad arguments provided to application. Expected syntax: ");
+                Console.WriteLine("BMFontOffset [source file path] [x offset] [y offset] [destination path]");
+                Console.WriteLine(e.Message);
+            }
+
+        }
+    }
+}
